9d03ab2e9ea39cefae62c6c4dbd2d373d30ac797
Prince Yadav <prince.yadav.tech26@gmail.com>
2026-09-25 23:04:31 +0530

    Delete olist_analysis_project.sql

--- olist_analysis_project.sql
+++
@@ -1,8 +0,0 @@
--- How many unique orders has the Olist e-commerce company received in total?
-
-use olist_project ;
-
-SELECT 
-    COUNT(DISTINCT order_id) AS total_order
-FROM
-    olist_orders_dataset;
